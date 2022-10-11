81970f34b4b4beca65764cb1be1715ae7d6ba620
Ali Pashayev <apashayev14102@ada.edu.az>
2022-10-12 00:19:13 +0400

    hw

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+var arr = [];
+// do{
+let quest = prompt('Username:');
+// for(var i = 0; i<arr.length; i++){
+//     if(quest != arr[i]){
+//     arr.push(arr[i]);
+//     alert("Ugurla elave olundu");
+//     }
+//     else alert("Bele bir username movcuddur");
+//     }}while(true);
+
+    function pushFunction(){
+        arr.push(quest);
+    }
+    function check(){
+        for(var i = 0; i<arr.length; i++){
+            if(quest != arr[i]){
+                function pushFunction(quest);
+                alert("Ugurla add olundu");
+            }
+            else alert("Bele bir ad movcuddur");
+
+        }
+    }
+
+
+
+ 
